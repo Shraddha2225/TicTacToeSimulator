2ac0e414e5401d05e11907df02f76e7501c6499e
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-06 10:41:22 +0530

    ADDED: Resetting The Player Board

--- TicTacToeProgram.sh
+++ TicTacToeProgram.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
 #welcome message 
 echo "WELCOME TIC TAC TOE SIMULATOR"
+
+#declare dictionary 
+declare -a  board
+
+board=(1 2 3 4 5 6 7 8 9)
+
+#function to displays Board
+function displayBoard()
+{
+	echo "  ${board[0]}   | ${board[1]}  | ${board[2]} "
+   echo "  ____|____|____"
+   echo "  ${board[3]}   | ${board[4]}  | ${board[5]}  "
+   echo "  ____|____|____"
+   echo "  ${board[6]}   | ${board[7]}  | ${board[8]}  "
+   echo "      |    |    "
+}
+displayBoard
